b0dac8ffdc5f7c2a04e25ffad339ee2e363e8d79
aayu2003 <136253507+aayu2003@users.noreply.github.com>
2023-09-29 19:36:22 +0530

    Add files via upload

--- Camera.py
+++ Camera.py
@@ -4,6 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Welcome to the API'
+    return jsonify('Welcome to the API'),200
 
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    # Check for the API key in the request headers
+    api_key = request.headers.get('X-API-Key')
 
+    # Replace 'YOUR_API_KEY' with your actual API key
+    if api_key == 'YOUR_API_KEY':
+        data = {'message': 'This is the protected data.'}
+        return jsonify(data), 200
+    else:
+        return jsonify({'error': 'Invalid API key'}), 401
+
+if __name__ == '__main__':
+    app.run()
